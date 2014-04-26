fe2b6cf21531bc2e6c7125843c013d37b5dc9ac5
crswin5726 <crswin5726@ung.edu>
2014-04-26 11:23:28 -0400

    Took out age field for Alumni model.  After thinking about which fields were needed I felt that age was not necessary.

--- managealumni/models.py
+++ managealumni/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Alumni(models.Model):
     name = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
-    age = models.IntegerField()
     year = models.IntegerField()
     track = models.CharField(max_length=30)
     lfw = models.BooleanField(default=False)
@@ -16,7 +15,6 @@ class Alumni(models.Model):
     def __unicode__(self):
         return "{} - {} - {} - {} - Track: {} Looking For Work: {}".format(self.name,
                                                                            self.email,
-                                                                           self.age,
                                                                            self.year,
                                                                            self.track,
                                                                            self.lfw)
